797988a1224cc3946492c2ca4992ad709a4f3c8a
Jess on the Hope <jesshon.py326@gmail.com>
2023-05-22 07:40:16 -0400

    created sell screen

--- app/src/main/java/com/bitpunchlab/android/barter/sell/SellScreen.kt
+++ app/src/main/java/com/bitpunchlab/android/barter/sell/SellScreen.kt
@@ -1,9 +1,58 @@
 package com.bitpunchlab.android.barter.sell
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.bitpunchlab.android.barter.R
+import com.bitpunchlab.android.barter.base.BottomBarNavigation
+import com.bitpunchlab.android.barter.base.TitleText
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SellScreen(navController: NavHostController) {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = { BottomBarNavigation(navController) }
+
+        ) {
+            Image(
+                painter = painterResource(id = R.mipmap.healthcheck),
+                modifier = Modifier
+                    .padding(top = 40.dp)
+                    .width(120.dp),
+                contentDescription = "Selling page icon"
+            )
+
+            TitleText(
+                title = "Sell",
+                modifier = Modifier
+                    .padding(top = 30.dp)
+            )
+
+            // a form to get the product's detail
+
+        }
+    }
+} 
+
+// product category, name, images, asked products (3), selling duration (like how many days)
+// 
+@Composable
+fun ProductForm() {
 
 }
